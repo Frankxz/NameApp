237e8ab6400978ebb2417deed0b64b7777049379
Miller <guf302010@gmail.com>
2021-09-18 15:21:59 +0700

    Initial Commit

---
+++ NameApp/NameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameAppApp.swift
+//  NameApp
+//
+//  Created by Robert Miller on 18.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct NameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
